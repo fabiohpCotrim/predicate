5fa08a04c2015a0d28e4a1f08e0d6d98e3bbc637
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-20 10:18:15 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,12 +29,14 @@ public class Product {
 	public static boolean staticProductPredicate(Product p) {
 		return  p.getPrice() >= 100.0;
 	}
+	
+	public boolean nonStaticProductPredicate() {
+		return  price >= 100.0;
+	}
 
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
 	}
-	
-	
 
 }
