891f574ccc309ab50174f5e250796e8c0bb28c54
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-20 10:22:14 -0300

    Expressão lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -17,8 +18,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
 
-		list.removeIf(Product::nonStaticProductPredicate);
+		list.removeIf(pred);
 
 		for (Product p : list) {
 			System.out.println(p);
